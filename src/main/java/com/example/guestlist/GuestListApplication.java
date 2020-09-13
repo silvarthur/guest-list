ba25aedb8d0d96c1056f63fc3b6faeb7bad0a808
Arthur Antunes Gonçalves Dias Silva <silvarthur@Arthurs-MacBook-Air.local>
2020-09-13 16:10:40 -0300

    first commit

---
+++ src/main/java/com/example/guestlist/GuestListApplication.java
@@ -0,0 +1,13 @@
+package com.example.guestlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guestlist/GuestListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guestlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
